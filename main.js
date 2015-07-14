231a8fc0c55821b770841a91d8a36d1f816a3e37
ahanlon <hanlonad27@gmail.com>
2015-07-14 12:15:05 -0600

    added click functionality and started ride table

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ var Ride = function(name, style, speed, time, distance, gain, description){
 }
 
 
-$( ".new-ride" ).submit(function( event ) {
-  console.log( $( this ).serializeArray() );
-  event.preventDefault();
-});
+// $( ".new-ride" ).submit(function( event ) {
+//   console.log( $( this ).serializeArray() );
+//   event.preventDefault();
+// });
 
 
 
@@ -27,6 +27,43 @@ var RideGoal = function(style, speed, time, distance, gain){
 	this.time 			= distance;
 	this.gain			= gain;
 }
+
+$('.btn').on('click', function(event){
+	event.preventDefault()
+});
+
+// hide main choice box and show set goal form
+
+$('.choose-ride').on('click', function(){
+	console.log("clicked");
+	$('.choice-box').removeClass('show').addClass('hide')
+	$('.new-goal').removeClass('hide')
+
+});
+
+// submit goals, hide the form and show the matching list of rides
+$('.goal-submit').on('click', function(){
+	console.log("goal submitted");
+	$('.new-goal').removeClass('show').addClass('hide')
+	$('.ride-list').addClass('show')
+
+});
+
+// hide main choice box and show set goal form
+
+$('.log-ride').on('click', function(){
+	console.log("clicked");
+	$('.choice-box').removeClass('show').addClass('hide')
+	$('.new-ride').removeClass('hide')
+});
+
+// submit the new ride log and return to main choice box
+$('.log-submit').on('click', function(){
+	console.log("log submitted");
+	$('.new-ride').addClass('hide')
+	$('.choice-box').removeClass('hide').addClass('show')
+
+});
 
 
 
