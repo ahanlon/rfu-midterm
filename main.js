a1dc5567cc0d95cfa82dffbcee44acb43a20bf56
ahanlon <hanlonad27@gmail.com>
2015-07-15 18:58:22 -0600

    added ride detail light box, working on map api

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(document).on('ready', function() {
 
+
+
 // this creates the ride object for each ride logged.
 
 var Ride = function(name, style, speed, time, distance, gain, description){
@@ -19,14 +21,15 @@ $( ".goal-submit" ).on(function( event ) {
   event.preventDefault();
 });
 
-
+var rideList = [];
 
 // this creates the goal object used when choosing a ride
 
 var RideGoal = function(style, speed, time, distance, gain){
 	this.style			= style;
 	this.speed			= speed;
-	this.time 			= distance;
+	this.time 			= time;
+	this.distance 		= distance;
 	this.gain			= gain;
 }
 
@@ -54,12 +57,16 @@ $('.goal-submit').on('click', function(){
 	var rideStyle = $('#rideStyle').val();
 	var rideSpeed = $('#rideSpeed').val();
 	var rideTime = $('#rideTime').val();
-	var rideDistance = $('#rideDistance');
-	var rideElevation = $('#rideElevation');
+	var rideDistance = $('#rideDistance').val();
+	var rideElevation = $('#rideElevation').val();
 	
 	var newGoal = new RideGoal(rideStyle, rideSpeed, rideTime, rideDistance, rideElevation);
+	rideList.push(newGoal)
+	console.log(newGoal);
+	console.log(rideList);	
 });
-// console.log(newGoal);
+
+
 
 // if cancel is clicked on new goal
 $('.goal-cancel').on('click', function(){
@@ -92,10 +99,80 @@ $('.log-cancel').on('click', function(){
 	$('.new-ride').removeClass('show').addClass('hide')
 });
 
+// Click on Domestique in header to reset show the choice-box and hide everything else
+
+$('.navbar-brand').on('click', function(){
+	console.log("clicked");
+	$('.choice-box').removeClass('hide').addClass('show')
+	$('.new-ride').removeClass('show').addClass('hide')
+	$('.new-goal').removeClass('show').addClass('hide')
+	$('.ride-list').removeClass('show').addClass('hide')
+	$('.rideDetail').removeClass('show').addClass('hide')
+
+});
+
+// ----- Show Ride Detail ----------------
+
+$('.name').on('click', function(){
+	$('.rideDetail').removeClass('hide').addClass('show')
+});
+
+$('.close-button').on('click', function(){
+	$('.rideDetail').removeClass('show').addClass('hide')
+});
 
 
+// ------------  Google Maps ------------------
 
+var map;
 
+function initialize() {
+  var mapOptions = {
+    zoom: 6
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5.'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
